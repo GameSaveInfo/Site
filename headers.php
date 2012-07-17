a7d807fc90554bbaec063b6a6d028a6a51beb6b6
Matthew Barbour <sanmadjack@gmail.com>
2012-07-17 08:50:05 -0500

    Added automatic symlinker to parent folder's lib dir

--- headers.php
+++ headers.php
@@ -1,7 +1,15 @@
 <?php
     // Includes 
     $folder =  dirname(__FILE__);
-    require_once $folder.'/../libs/geshi/geshi.php';
+        if(!is_dir($folder.'/libs/')) {
+        if(is_dir($folder.'/../libs/')) {
+            // Means there is a lib folder up above!
+            symlink($folder.'/../libs/',$folder.'/libs');
+        } else {
+            throw new Exception("Lib folder not locatable!");
+        }
+    }
+    require_once $folder.'/libs/geshi/geshi.php';
     include_once $folder.'/../config.php';
 
     global $gdb;
