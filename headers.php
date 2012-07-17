0e49fdfed4f76293ba6d0cbe4b02c48d1b6c4f8c
Matthew Barbour <sanmadjack@gmail.com>
2012-07-17 09:11:32 -0500

    removed symlink code

--- headers.php
+++ headers.php
@@ -1,14 +1,6 @@
 <?php
     // Includes 
     $folder =  dirname(__FILE__);
-        if(!is_dir($folder.'/libs/')) {
-        if(is_dir($folder.'/../libs/')) {
-            // Means there is a lib folder up above!
-            symlink($folder.'/../libs/',$folder.'/libs');
-        } else {
-            throw new Exception("Lib folder not locatable!");
-        }
-    }
     require_once $folder.'/libs/geshi/geshi.php';
     include_once $folder.'/../config.php';
 
