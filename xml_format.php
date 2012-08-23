a295fe78fbaa7b6a174476232b7dc75ec6b8c668
Matthew Barbour <sanmadjack@gmail.com>
2012-08-23 16:22:08 -0500

    Added more description for linkable tags

--- xml_format.php
+++ xml_format.php
@@ -651,8 +651,10 @@ It can use the exact same attributes as a "save" element (described above).</p>
 
 <p>Some games can handle having their save folders symlinked to another folder, like inside of a Dropbox or Google Drive folder.
 Do this on two computers, and you've got their saves automatically synced!
-You can specify paths that allow for this using this.
-Individual files are not supported here, as Windows freaks the hell out if you try.
+The "linkable" element allows you to explain how to link a particular game.
+This process only works reliably with folders, so that's the only thing you can specify.
+If you specify a path, it will append that path to any detected locations.
+If no path, then the detected location itself will be used.
 </p>
 
 <h2><li>PlayStation Games</li></h2>
